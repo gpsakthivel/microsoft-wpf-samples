e1607015c6c874a4ad3c329f3745ef8ab28b13e2
gpsakthivel <psakthi24vel@gmail.com>
2023-07-30 18:56:38 +0530

    Added the bold reports sample projects

---
+++ bold-reports-wpf-report-viewer/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace bold_reports_wpf_report_viewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += new RoutedEventHandler(MainWindow_Loaded);
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.reportViewer.ReportPath = System.IO.Path.Combine(Environment.CurrentDirectory, @"Resources\sales-order-detail.rdl");
+            this.reportViewer.RefreshReport();
+        }
+    }
+}
---
+++ bold-reports-wpf-report-writer/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BoldReports.Writer;
+
+namespace bold_reports_wpf_report_writer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Refer the RDL report file location
+                string reportPath = (@"<root folder>\Resources\sales-order-detail.rdl");
+
+                string fileName = null;
+                WriterFormat format;
+
+                //Step 1 : Instantiate the report writer with the parameter "ReportPath".
+                ReportWriter reportWriter = new ReportWriter(reportPath);
+                //Step 2 : Save the report as Pdf or Word or Excel
+                if (pdf.IsChecked == true)
+                {
+                    fileName = "sales-order-detail.pdf";
+                    format = WriterFormat.PDF;
+                }
+                else if (word.IsChecked == true)
+                {
+                    fileName = "sales-order-detail.docx";
+                    format = WriterFormat.Word;
+                }
+                else if (excel.IsChecked == true)
+                {
+                    fileName = "sales-order-detail.xlsx";
+                    format = WriterFormat.Excel;
+                }
+                else
+                {
+                    fileName = "sales-order-detail.html";
+                    format = WriterFormat.HTML;
+                }
+                reportWriter.Save(fileName, format);
+                //Message box confirmation to view the created report document.
+                if (MessageBox.Show("Do you want to view the " + format + " file?", "" + format + " report Created",
+                    MessageBoxButton.YesNo, MessageBoxImage.Information) == MessageBoxResult.Yes)
+                {
+                    //Launching the PDF file using the default Application.[Acrobat Reader]
+                    System.Diagnostics.Process.Start(fileName);
+                }
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message);
+            }
+        }
+    }
+}
